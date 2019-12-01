3be6c443b85cec639d218d7f47ad0ae452d4bd6b
Soltox <pascal.schaller@outlook.com>
2019-12-01 12:58:54 +0100

    Initial commit

---
+++ src/main/java/rocks/process/acrm/DigiprAcrmApiDocApplication.java
@@ -0,0 +1,13 @@
+package rocks.process.acrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiprAcrmApiDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiprAcrmApiDocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rocks/process/acrm/DigiprAcrmApiDocApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.process.acrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiprAcrmApiDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
